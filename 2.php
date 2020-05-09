9e25ebb17515eec33c01133d0d816745504fab13
rifanid98 <adninsijawa.office@gmail.com>
2020-05-09 10:33:56 +0700

    menambahkan file 2.php

---
+++ 2.php
@@ -0,0 +1,26 @@
+
+<?php
+
+function usernameValidity($username){
+    $preg_match = "/^[a-z]{1}+[a-z._0-9]{7,11}$/";
+    if(preg_match($preg_match, $username)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+var_dump(usernameValidity("john.smith"));
+var_dump(usernameValidity("johnsmith26"));
+var_dump(usernameValidity("JOHNSmith"));
+
+function passwordValidity($password){
+    $preg_match = "/^(?=.*[0-9])(?=.*[!”#\$%&’()*+,-.\/:;<=>?@\[\]\^_\`{|}~\\\])[a-zA-Z0-9!”#\$%&’()*+,-.\/:;<=>?@\[\]\^_\`{|}~\\\]{9}$/";
+    if(preg_match($preg_match, $password)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+var_dump(passwordValidity("j0hnam!th"));
